342b11991bcc422017ff8e50b80617d16d38d88f
William Bendix Pedersen <william.bendix@live.dk>
2020-04-01 16:13:26 +0200

    Succesful container registration stepdefinition

--- src/test/java/StepDefinition.java
+++ src/test/java/StepDefinition.java
@@ -1,5 +1,47 @@
 import io.cucumber.java.en.*;
 
 public class StepDefinition {
+	
+	@Given("Port of origin {string}")
+	public void port_of_origin(String string) {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+	
+	@Given("Destination {string}")
+	public void destination(String string) {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+	
+	@Given("Client {string}")
+	public void client(String string) {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+	
+	@Given("Content {string}")
+	public void content(String string) {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+	
+	@Given("Container in-port\"Stockholm\" and {string}")
+	public void container_in_port_Stockholm_and(String string) {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+	
+	@When("Registering a container")
+	public void registering_a_container() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+	
+	@Then("Change container status to {string}")
+	public void change_container_status_to(String string) {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
 
 }
