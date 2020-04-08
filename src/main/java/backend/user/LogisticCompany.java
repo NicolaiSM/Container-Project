b2db79df92369eb17cba4946218dc2c78c600326
Oliver Kaupert <oliverkaupert@student.dtu.dk>
2020-04-08 11:59:48 +0200

    logisticCompany

---
+++ src/main/java/backend/user/LogisticCompany.java
@@ -0,0 +1,13 @@
+package backend.user;
+
+public class LogisticCompany extends GeneralUser {
+
+	public LogisticCompany(String companyname, String companyaddress, String contactperonsname,
+			String contactperonsemail) {
+		super(companyname, companyaddress, contactperonsname, contactperonsemail);
+
+	}
+	
+
+
+}
